2ce9b5ec29b11262074b800aa6dc70b9f273c119
EugineAgolla <agollaeugine@gmail.com>
2025-02-19 19:11:21 +0300

    adds testimonial section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,4 @@
  import Hero from "../components/Hero"
-import Products from "./Products"
 import Services from "../components/Services"
 import Reviews from "./Reviews"
 
@@ -8,10 +7,9 @@ export default function Home(){
     return(
         <>
         <Hero />
-        <h1 className="text-4xl text-sky-500 justify-center text-center  font-serif">Featured Collections</h1>
-        <Products />
-        <h1 className="text-4xl text-sky-500 justify-center text-center  font-serif">Our Services</h1>
         <Services />
+        <h1 className="text-4xl text-sky-500 justify-center text-center  font-serif">Featured Collections</h1>
+        <Reviews />
         </>
     )
 }
