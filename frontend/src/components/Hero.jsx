eb5e8453bbee2f65275b1c3590b6029a815f9b1b
EugineAgolla <agollaeugine@gmail.com>
2025-02-21 17:17:03 +0300

    updated hero

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,15 +1,22 @@
+import { useNavigate } from "react-router-dom";
 const Hero = () => {
+const Navigate = useNavigate();
+
+  const handleClick = () => {
+    Navigate("/products");
+  };
+
   return (
     <div className="bg-[url('img/BG.png')] bg-cover bg-no-repeat w-full h-screen px-6 md:px-12 lg:px-24 flex flex-col lg:flex-row items-center justify-center lg:justify-around text-center lg:text-left">
       <div className="max-w-lg">
         <h1 className="font-lemon text-4xl md:text-5xl text-black capitalize leading-tight">
-          get fresh <span className="text-[#ef5e41]">juiceables</span> everyday
+        Stitching love  <span className="text-[#ef5e41]">into</span> every loop. 🌿 Where yarn meets art.
         </h1>
         <p className="font-opensans text-base md:text-lg text-gray-800 py-4 md:py-6">
-          starting at $360 per month, get those benefits and more.
+        🔥Handmade is the new luxury. 🧶 Knots, loops, and endless possibilities.
         </p>
-        <button className="font-opensans-bold text-white text-lg w-32 md:w-36 h-12 md:h-16 rounded-[50px_0px_50px_0px] bg-[#ef5e41] border-none">
-          Details
+        <button onClick={handleClick} className="font-opensans-bold text-white text-lg w-32 md:w-36 h-12 md:h-16 rounded-[50px_0px_50px_0px] bg-[#ef5e41] border-none">
+          Shop Now
         </button>
       </div>
       <div className="w-60 md:w-96 lg:w-[500px] h-60 md:h-96 lg:h-[500px] mt-8 lg:mt-0">
